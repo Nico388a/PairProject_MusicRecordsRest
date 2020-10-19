05421a28655fce5047b265d764d914a04c2ca4e5
Nicolai Höyer Christiansen <nico388a@edu.easj.dk>
2020-10-19 11:32:07 +0200

    Starting List

---
+++ PairProject_MusicRecordsRest/Controllers/MusicRecordsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PairProject_MusicRecordsRest.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PairProject_MusicRecordsRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicRecordsController : ControllerBase
+    {
+        static List<MusicRecord> liste = new List<MusicRecord>()
+        {
+            new MusicRecord("Boom", "Slash", 1680, 1997),
+            new MusicRecord("My Man", "Christiana", )
+        };
+        // GET: api/<MusicRecordsController>
+        [HttpGet]
+        public IEnumerable<MusicRecord> Get()
+        {
+            return 
+        }
+
+        // GET api/<MusicRecordsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MusicRecordsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MusicRecordsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MusicRecordsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PairProject_MusicRecordsRest/Model/MusicRecord.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PairProject_MusicRecordsRest.Model
+{
+    public class MusicRecord
+    {
+        private string _title;
+        private string _artist;
+        private int _duration;
+        private int _yearOfPublication;
+
+        public string Title
+        {
+            get => _title;
+            set => _title = value;
+        }
+
+        public string Artist
+        {
+            get => _artist;
+            set => _artist = value;
+        }
+
+        public int Duration
+        {
+            get => _duration;
+            set => _duration = value;
+        }
+
+        public int YearOfPublication
+        {
+            get => _yearOfPublication;
+            set => _yearOfPublication = value;
+        }
+
+        public MusicRecord()
+        {
+            
+        }
+
+        public MusicRecord(string title, string artist, int duration, int yearOfPublication)
+        {
+            _title = title;
+            _artist = artist;
+            _duration = duration;
+            _yearOfPublication = yearOfPublication;
+        }
+    }
+}
