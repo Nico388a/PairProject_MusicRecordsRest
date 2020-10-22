6d55cc42bc5227efb0c8d0b6567fe0fb3ad0140b
Nicolai Höyer Christiansen <nico388a@edu.easj.dk>
2020-10-22 12:44:01 +0200

    selenium

--- PairProject_MusicRecordsRestTests/MusicRecordsUITest.cs
+++ PairProject_MusicRecordsRestTests/MusicRecordsUITest.cs
@@ -4,13 +4,14 @@ using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace PairProject_MusicRecordsRestTests
 {
     [TestClass]
     public class MusicRecordsUITest
     {
-        private const string DIRECTORY = "C:\\Users\\Bruger\\SeliniumDrive";
+        private const string DIRECTORY = "C:\\Selenium drive";
         private static IWebDriver _driver;
 
         [ClassInitialize]
@@ -30,6 +31,11 @@ namespace PairProject_MusicRecordsRestTests
         {
             _driver.Navigate().GoToUrl("http://localhost:3000/");
             Assert.AreEqual("Hello app", _driver.Title);
+
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(30));
+            IWebElement musicList = wait.Until(d => d.FindElement(By.Id("musicList")));
+
+            Assert.IsTrue(musicList.Text.Contains("Boom"));
         }
     }
 }
